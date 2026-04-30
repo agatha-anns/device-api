2d8d08e2131a2872ceb3c17dbe53afab32da9e61
Agatha Anna Baby <agathamoolamkuzhiyil@gmail.com>
2026-04-30 12:21:06 +0200

    Add and save device

---
+++ src/main/java/com/device/device_api/controller/DeviceController.java
@@ -0,0 +1,28 @@
+package com.device.device_api.controller;
+
+import com.device.device_api.entity.Device;
+import com.device.device_api.service.DeviceService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("devices")
+public class DeviceController {
+    private final DeviceService service;
+
+    public DeviceController(DeviceService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Device create(@RequestBody Device device){//create new device
+        return service.save(device);
+    }
+
+    @GetMapping
+    public List<Device> getAll(){// gett all devices
+
+        return service.getAll();
+    }
+}
--- src/main/java/com/device/device_api/service/DeviceService.java
+++ src/main/java/com/device/device_api/service/DeviceService.java
@@ -1,4 +1,24 @@
 package com.device.device_api.service;
 
+import com.device.device_api.entity.Device;
+import com.device.device_api.repository.DeviceRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class DeviceService {
+    private final DeviceRepository repository;
+
+    public DeviceService(DeviceRepository repository) {
+        this.repository = repository;
+    }
+
+    public Device save(Device device){//save new device
+        return repository.save(device);
+    }
+
+    public List<Device> getAll(){//get all devices
+        return repository.findAll();
+    }
 }
